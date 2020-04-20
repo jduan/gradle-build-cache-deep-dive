90d98d0782e7de07012662a6a16c19a7938a9d41
Jingjing Duan <duanjingjing@gmail.com>
2020-04-20 09:20:39 -0700

    init commit

---
+++ labs/04-handling-cache-misses/application/src/main/java/com/example/application/Main.java
@@ -0,0 +1,26 @@
+package com.example.application;
+
+import com.example.gradle.info.BuildInfo;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Hello World");
+            System.out.println(new BuildInfo().toString());
+            Arrays.asList(args).forEach(arg -> {
+                try {
+                    Files.readAllLines(Paths.get(arg)).forEach(System.out::println);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
